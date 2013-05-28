4def15d4eb7e403d2c38c59f2547428a7fd45f73
Luis Quesada <elezeta@gmail.com>
2013-05-28 11:25:56 +0200

    Renamed ParserGenerator to ParserFactory and LexerGenerator to LexerFactory.

---
+++ ModelCC/test/test/languages/wronglanguages/WrongClass23.java
@@ -0,0 +1,21 @@
+/*
+ * ModelCC, under ModelCC Shared Software License, www.modelcc.org. Luis Quesada Torres.
+ */
+
+
+package test.languages.wronglanguages;
+
+import org.modelcc.*;
+
+/**
+ *
+ * @author elezeta
+ */
+@Pattern(regExp="]]]]]]]]]]")
+public class WrongClass23 implements IModel {
+
+    private WrongClass23() {
+
+    }
+
+}
