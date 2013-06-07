5b4d9d1d26e69bfaf9ba2dc7dc205dd376d2e09f
Luis Quesada <luis@elezeta.com>
2013-06-07 16:00:30 +0200

    Updated headers

--- ModelCC/test/test/languages/emptymatchers/complex/ContentComplex.java
+++ ModelCC/test/test/languages/emptymatchers/complex/ContentComplex.java
@@ -1,7 +1,16 @@
+/*
+ * ModelCC, under ModelCC Shared Software License, www.modelcc.org. Luis Quesada Torres.
+ */
+
+
 package test.languages.emptymatchers.complex;
 
 import org.modelcc.*;
 
+/**
+ *
+ * @author elezeta
+ */
 public class ContentComplex extends Content implements IModel {
 
 	public Basic empty;
--- ModelCC/test/test/languages/emptymatchers/prefix2/ContentComplex.java
+++ ModelCC/test/test/languages/emptymatchers/prefix2/ContentComplex.java
@@ -1,7 +1,16 @@
+/*
+ * ModelCC, under ModelCC Shared Software License, www.modelcc.org. Luis Quesada Torres.
+ */
+
+
 package test.languages.emptymatchers.prefix2;
 
 import org.modelcc.*;
 
+/**
+ *
+ * @author elezeta
+ */
 @Prefix("c")
 public class ContentComplex extends Content implements IModel {
 
