ff69b5a670dffad3033fc0e37e6c446bc18c177f
Luis Quesada <luis@elezeta.com>
2013-07-29 11:29:10 +0200

    Added probabilistic model classes

---
+++ ModelCC/src/org/modelcc/Probability.java
@@ -0,0 +1,29 @@
+/*
+ * ModelCC, under ModelCC Shared Software License, www.modelcc.org. Luis Quesada Torres.
+ */
+
+
+package org.modelcc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.modelcc.probabilistic.ProbabilityEvaluator;
+
+/**
+ * Probability.
+ * @author elezeta
+ */
+@Target({ElementType.TYPE,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Probability {
+
+	public double p() default -1;
+	
+	public int frequency() default -1;
+	
+	public Class evaluator() default Probability.class;
+	
+}
---
+++ ModelCC/src/org/modelcc/probabilistic/ProbabilityEvaluator.java
@@ -0,0 +1,14 @@
+/*
+ * ModelCC, under ModelCC Shared Software License, www.modelcc.org. Luis Quesada Torres.
+ */
+
+
+package org.modelcc.probabilistic;
+
+/**
+ * Probability evaluator.
+ * @author elezeta
+ */
+public abstract class ProbabilityEvaluator {
+	
+}
