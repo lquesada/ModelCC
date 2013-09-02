9d17846a78bed3a10a65aa573d1a11643d94ce0e
Luis Quesada <luis@elezeta.com>
2013-09-02 12:37:31 +0200

    Fixed NonQuotedStringModel pattern test

--- ModelCC/test/test/org/modelcc/types/StringTest.java
+++ ModelCC/test/test/org/modelcc/types/StringTest.java
@@ -71,7 +71,7 @@ public class StringTest {
 		checkMatches(StringModel.class,"a+",1);
 		checkMatches(StringModel.class,"+8\"919",1);
 		checkMatches(StringModel.class,"-",1);
-		checkMatches(StringModel.class,"a    asdiof",1);
+		checkMatches(StringModel.class,"aasdiof",1);
 		checkMatches(StringModel.class,"    asdiof",1);
 		checkMatches(StringModel.class,"a;asdf",0);
 		checkMatches(StringModel.class,"a;",0);
